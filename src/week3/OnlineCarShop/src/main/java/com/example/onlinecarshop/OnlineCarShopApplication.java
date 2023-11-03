af0c1b07c941ee322859b1af8c7e9e74e7c1e32c
teurem <teurem.cs@gmail.com>
2023-11-03 20:02:48 +0600

    dhjskhfsjhf

--- src/week3/OnlineCarShop/src/main/java/com/example/onlinecarshop/OnlineCarShopApplication.java
+++ src/week3/OnlineCarShop/src/main/java/com/example/onlinecarshop/OnlineCarShopApplication.java
@@ -1,11 +1,12 @@
 package com.example.onlinecarshop;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "backend API", version = "0.1.3", description = "basic functional"))
 public class OnlineCarShopApplication {
 
 	public static void main(String[] args) {
